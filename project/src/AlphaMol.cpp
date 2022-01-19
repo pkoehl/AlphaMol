c49d90ecd8c9f558413d3ffb7ce23754ccc8d9f5
pkoehl <46326280+pkoehl@users.noreply.github.com>
2022-01-19 12:35:38 -0800

    Update AlphaMol.cpp

--- project/src/AlphaMol.cpp
+++ project/src/AlphaMol.cpp
@@ -214,6 +214,9 @@ int main(int argc, char **argv)
 
 	}
 */
+	delete [] coord; delete [] radii; delete [] coefS; delete [] coefV; delete [] coefM; delete [] coefG;
+	delete [] ballwsurf; delete [] dsurf; delete [] ballwvol; delete [] dvol;
+	delete [] ballwmean; delete [] dmean; delete [] ballwgauss; delete [] dgauss;
 
 	return 0;
 
